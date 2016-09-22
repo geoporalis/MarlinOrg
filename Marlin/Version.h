7839fb436a72a76fb3dcfb6dde641dd2c9cd79dc
GP <georg.poller@gmail.com>
2016-09-22 10:50:44 +0200

    playing with build_date

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,7 +41,7 @@
    * Verbose version identifier which should contain a reference to the location
    * from where the binary was downloaded or the source code was compiled.
    */
-  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github)"
+  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION __DATE__ " " __TIME__
 
   /**
    * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
@@ -76,7 +76,7 @@
    * has a distinct Github fork— the Source Code URL should just be the main
    * Marlin repository.
    */
-  #define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+  #define SOURCE_CODE_URL "https://github.com/geoporalis/MarlinOrg"
 
   /**
    * Default generic printer UUID.
